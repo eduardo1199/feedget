59101766b8e8d0f575e708eba0d24fda3118a762
Eduardo Soares de Araujo Aquino <eduardo.soares.116@ufrn.edu.br>
2022-05-02 16:09:54 -0300

    create project feed

---
+++ web/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./src/**/*.tsx"],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          500: '#8257E5'
+        },
+        brandHover: {
+          500: '#996DFF'
+        },
+        textBrandColor: {
+          500: '#ffffff'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
